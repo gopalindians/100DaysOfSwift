8290383f9cb91e58535b53c8068fa12748dda6a1
Gopal Sharma <gopalindians@gmail.com>
2023-08-13 07:44:20 +0530

    Update 001.swift
    Added 2. Strings and integers

--- 001.swift
+++ 001.swift
@@ -1,6 +1,12 @@
 import SwiftUI
-//1.Variables
+
+//1. Variables
 var greeting = "Hello, playground"
 print(greeting);
 greeting = "HOLA";
 greeting = "NAMASTE"
+
+//2. Strings and integers
+var str = "Hello"
+var age = 50
+var population = 80_00_000
