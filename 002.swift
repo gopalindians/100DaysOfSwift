dc5b31bc394f4d0423d5e27ad1b0d39a0e9b8bad
Gopal Sharma <gopalindians@gmail.com>
2023-08-14 14:51:09 +0530

    Create 002.swift
    Added  1.Arrays & 2.Sets

---
+++ 002.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+//002 Complex data types
+
+//1. Arrays
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+let beatles = [john, paul, george, ringo]
+beatles[1]
+print(beatles[1]);
+
+//2. Sets
+let colors = Set(["red", "green", "blue"])
+print(colors)
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+print(colors2)
