5880d806404301893707f55278fd693dd99c1031
Gopal Sharma <gopalindians@gmail.com>
2023-08-15 16:30:13 +0530

    Update 003.swift
    //8.Switch statements

--- 003.swift
+++ 003.swift
@@ -80,3 +80,28 @@ if firstCard1 == secondCard2 {
 } else {
     print("Cards are different")
 }
+
+
+//8.Switch statements
+let weather = "sunny"
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+default:
+    print("Enjoy your day!")
+}
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
