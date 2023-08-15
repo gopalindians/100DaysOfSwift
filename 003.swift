ba13208ae8d3fac91b99a48c71706cc0510565d7
Gopal Sharma <gopalindians@gmail.com>
2023-08-15 16:11:23 +0530

    Create 003.swift
    //1. Arithmetic Operators

---
+++ 003.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+//003 – operators and conditions
+
+//1. Arithmetic Operators
+
+let firstScore = 12
+let secondScore = 4
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+let remainder = 13 % secondScore
