1160b1974090730ebd71cb932ec6b3ab9f07004d
Gopal Sharma <gopalindians@gmail.com>
2023-08-16 17:27:13 +0530

    Update 004.swift
    //5.Exiting multiple loops

--- 004.swift
+++ 004.swift
@@ -78,3 +78,24 @@ while countDown >= 0 {
 
     countDown -= 1
 }
+
+
+//5.Exiting multiple loops
+for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+    }
+}
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
