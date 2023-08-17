4617a399428051e30f67d53a66fb4fd9e058fdaf
Gopal Sharma <gopalindians@gmail.com>
2023-08-18 02:41:19 +0530

    Update 006.swift
    //3.Returning values from a closure

--- 006.swift
+++ 006.swift
@@ -16,3 +16,13 @@ let driving1 = { (place: String) in
 }
 driving1("London")
 
+
+//3.Returning values from a closure
+let driving2 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+let message = drivingWithReturn("London")
+print(message)
