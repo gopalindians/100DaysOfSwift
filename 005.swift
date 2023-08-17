f122708d8ef9bf79e6d6bc5fad960dd6c836f37d
Gopal Sharma <gopalindians@gmail.com>
2023-08-17 15:20:13 +0530

    Create 005.swift
    //1. Writing functions

---
+++ 005.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+//005 – functions, parameters, and errors
+
+//1. Writing functions
+
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+
+printHelp()
+
+func readUserInput() {
+    while true {
+        print("Reading user input...")
+        break
+    }
+}
+readUserInput()
