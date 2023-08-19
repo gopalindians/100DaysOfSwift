d9f60156b183982f99a9d9911435ba6abc4e09d8
Gopal Sharma <gopalindians@gmail.com>
2023-08-19 23:46:32 +0530

    Update 008.swift
    //004. Methods

--- 008.swift
+++ 008.swift
@@ -9,3 +9,63 @@ struct Sport {
 var tennis = Sport(name: "Tennis")
 print(tennis.name)
 tennis.name = "Lawn tennis"
+
+
+
+
+//002 Computed properties
+
+struct Sport1 {
+    var name: String
+}
+
+
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+
+//003. Property observers
+
+struct Progress {
+    var task: String
+    var amount: Int
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct Progress1 {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+
+//004. Methods
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+let london = City(population: 9_000_000)
+london.collectTaxes()
