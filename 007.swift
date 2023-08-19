0f8af8766287a2b562fcb4b395192aa1302ad05e
Gopal Sharma <gopalindians@gmail.com>
2023-08-19 13:43:40 +0530

    Update 007.swift
    //5.Returning closures from functions

--- 007.swift
+++ 007.swift
@@ -33,3 +33,54 @@ func travel1(action: (String) -> String) {
 travel1 { (place: String) -> String in
     return "I'm going to \(place) in my car"
 }
+
+
+//3.Shorthand parameter names
+
+
+func travel2(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel2 { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+travel2 { place -> String in
+    return "I'm going to \(place) in my car"
+}
+travel2 { place in
+    "I'm going to \(place) in my car"
+}
+
+travel2 {
+    "I'm going to \($0) in my car"
+}
+
+
+
+//4.Closures with multiple parameters
+func travel3(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+
+travel3 {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+
+//5.Returning closures from functions
+func travel5() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let result5 = travel5()
+result5("India")
