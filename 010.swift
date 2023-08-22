53598cb5c595da4c38d4b90028db3a8a7feb0f10
Gopal Sharma <gopalindians@gmail.com>
2023-08-22 23:53:55 +0530

    Update 010.swift
    //002 Class inheritance

--- 010.swift
+++ 010.swift
@@ -14,3 +14,21 @@ class Dog{
 }
 
 let poppy =  Dog(name: "Poppy", breed: "Poddy");
+
+//002 Class inheritance
+
+class Dog1 {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+class Poodle2: Dog1 {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
