995068c945cdc2356d7fa34b5f3e39394f115ebf
Gopal Sharma <gopalindians@gmail.com>
2023-08-25 01:25:57 +0530

    Create 011.swift
    //001 Protocols

---
+++ 011.swift
@@ -0,0 +1,17 @@
+
+import SwiftUI
+//011 – Day 11:  protocols, extensions, and protocol extensions
+
+//001 Protocols
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
