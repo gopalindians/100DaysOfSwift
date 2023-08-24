721c59f84f9ab10f5ddeb59340e911bc80170fc0
Gopal Sharma <gopalindians@gmail.com>
2023-08-25 01:47:47 +0530

    Update 011.swift
    //004 Protocol extensions

--- 011.swift
+++ 011.swift
@@ -50,3 +50,21 @@ extension Int {
     }
 }
 number.isEven
+
+
+//004 Protocol extensions
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
