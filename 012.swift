5c50b67c764e7b45a6807b89a62729681e42215a
Gopal Sharma <gopalindians@gmail.com>
2023-08-25 15:33:57 +0530

    Update 012.swift
    //002 Unwrapping optionals

--- 012.swift
+++ 012.swift
@@ -9,3 +9,12 @@ age = 38
 
 var income: Int? = nil
 income = 38_00_000
+
+
+//002 Unwrapping optionals
+var name: String? = nil
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
