0ce309067092f595fd5ecce956af6f8a8a52833a
Gopal Sharma <gopalindians@gmail.com>
2023-08-25 15:30:28 +0530

    Create 012.swift
    //001 Handling missing data

---
+++ 012.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+//012 – Day 12:  – optionals, unwrapping, and typecasting
+
+//001 Handling missing data
+
+var age: Int? = nil
+age = 38
+
+var income: Int? = nil
+income = 38_00_000
