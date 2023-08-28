33ec7f64fcaafb7274dfd2a24d2e9ed4ac52b717
Gopal Sharma <gopalindians@gmail.com>
2023-08-28 10:54:21 +0530

    Update 013.swift
    //002 Types of Data

--- 013.swift
+++ 013.swift
@@ -1,8 +1,54 @@
+
 import SwiftUI
 //013 – Day 13 – Swift review, day one
 
 //001 Variables and constants
 
 var name = "Tim McGraw"
-let name = "Tim McGraw"
+//let name = "Tim McGraw"
 name = "Romeo"
+
+
+//002 Types of Data
+
+//var name
+//name = "Tim McGraw"
+//
+//var name: String
+//name = "Tim McGraw"
+
+var name1: String
+name1 = "Tim McGraw"
+
+var age: Int
+age = 25
+//name1 = 25
+//age = "Tim McGraw"
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var longitude1: Double
+
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+var name2: String
+name2 = "Tim McGraw"
+var name3: String = "Tim McGraw"
